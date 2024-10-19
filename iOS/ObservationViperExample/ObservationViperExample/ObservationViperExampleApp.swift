1f301245ec024e5560b0af990a7fea2d0b9c9048
fumiyasac <just1factory@gmail.com>
2024-10-19 15:24:02 +0900

    initial commit

---
+++ iOS/ObservationViperExample/ObservationViperExample/ObservationViperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationViperExampleApp.swift
+//  ObservationViperExample
+//
+//  Created by 酒井文也 on 2024/10/19.
+//
+
+import SwiftUI
+
+@main
+struct ObservationViperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/ObservationViperExample/ObservationViperExampleTests/ObservationViperExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationViperExampleTests.swift
+//  ObservationViperExampleTests
+//
+//  Created by 酒井文也 on 2024/10/19.
+//
+
+import Testing
+@testable import ObservationViperExample
+
+struct ObservationViperExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
