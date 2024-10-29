e1ad55f25a031967dc70a59ff6018873e83ef863
fumiyasac <just1factory@gmail.com>
2024-10-29 09:31:29 +0900

    エンドポイントを変更する

--- iOS/ObservationViperExample/ObservationViperExample/Infrastructure/APIClientManager.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Infrastructure/APIClientManager.swift
@@ -47,8 +47,8 @@ final class ApiClientManager {
 
     private enum EndPoint: String {
 
-        case pickupFoods = "pickup_foods"
-        case popularFoods = "popular_foods"
+        case foodPhotos = "pickup_foods"
+        case recentNews = "recent_news"
 
         func getBaseUrl() -> String {
             return [host, self.rawValue].joined(separator: "/")
