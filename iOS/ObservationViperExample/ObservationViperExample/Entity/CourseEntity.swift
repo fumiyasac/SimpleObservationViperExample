4ea1170d46703baa197557e63b99e5f27c400a6b
fumiyasac <just1factory@gmail.com>
2024-11-13 15:19:39 +0900

    Entityの雛形を考えないと...

--- iOS/ObservationViperExample/ObservationViperExample/Entity/CourseEntity.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Entity/CourseEntity.swift
@@ -5,3 +5,6 @@
 //  Created by 酒井文也 on 2024/11/11.
 //
 
+struct CourseEntity: Hashable {
+    // TODO: 必要なPropertyをこれから決めていく
+}
--- iOS/ObservationViperExample/ObservationViperExample/Entity/FeaturedEntity.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Entity/FeaturedEntity.swift
@@ -5,3 +5,6 @@
 //  Created by 酒井文也 on 2024/11/11.
 //
 
+struct FeaturedEntity: Hashable {
+    // TODO: 必要なPropertyをこれから決めていく
+}
