d11dc562df68bfbb0c6826db7a8aafcfc0f12440
fumiyasac <just1factory@gmail.com>
2024-11-20 22:21:12 +0900

    Login用のViewファイルを追加

---
+++ iOS/ObservationViperExample/ObservationViperExample/Screen/Authenticate/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ObservationViperExample
+//
+//  Created by 酒井文也 on 2024/11/20.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
