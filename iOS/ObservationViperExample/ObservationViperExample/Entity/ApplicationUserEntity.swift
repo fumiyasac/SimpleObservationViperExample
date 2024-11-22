353db0a0c487c53f51aaeb560399ad745df6c35d
fumiyasac <just1factory@gmail.com>
2024-11-22 23:32:30 +0900

    ApplicationUserEntityのファイルを新規追加

---
+++ iOS/ObservationViperExample/ObservationViperExample/Entity/ApplicationUserEntity.swift
@@ -0,0 +1,10 @@
+//
+//  ApplicationUserEntity.swift
+//  ObservationViperExample
+//
+//  Created by 酒井文也 on 2024/11/22.
+//
+
+struct ApplicationUserEntity: Hashable {
+    // TODO: 必要なPropertyをこれから決めていく
+}
