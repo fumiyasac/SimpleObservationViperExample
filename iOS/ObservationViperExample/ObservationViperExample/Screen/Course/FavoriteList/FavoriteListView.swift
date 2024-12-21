5b14512d4169d3481eff75444735b372edbde477
fumiyasac <just1factory@gmail.com>
2024-12-22 08:30:07 +0900

    FavoriteListViewの雛形を新規追加

---
+++ iOS/ObservationViperExample/ObservationViperExample/Screen/Course/FavoriteList/FavoriteListView.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteListView.swift
+//  ObservationViperExample
+//
+//  Created by 酒井文也 on 2024/12/22.
+//
+
+import SwiftUI
+
+// MEMO: CourseのBookMark一覧
+
+struct FavoriteListView: View {
+
+    // MARK: - Body
+
+    var body: some View {
+        Text("FavoriteListView")
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    FavoriteListView()
+}
