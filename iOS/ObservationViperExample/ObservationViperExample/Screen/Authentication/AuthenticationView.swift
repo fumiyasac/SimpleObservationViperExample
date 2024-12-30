f76eedd2136d042a23643b752af153036f19582d
fumiyasac <just1factory@gmail.com>
2024-12-30 13:56:06 +0900

    Login画面にNavigationを付与する

--- iOS/ObservationViperExample/ObservationViperExample/Screen/Authentication/AuthenticationView.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Screen/Authentication/AuthenticationView.swift
@@ -27,34 +27,39 @@ struct AuthenticationView: View {
     // MARK: - Body
 
     var body: some View {
-        // TODO: 本番のView要素を適用させる
-        VStack(spacing: 20) {
-            TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .autocapitalization(.none)
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-
-            // Loading状態の場合はIndicatorを表示し、そうでない時はログインボタンを表示する
-            if presenter.isLoading {
-                ProgressView()
-            } else {
-                Button("Login") {
-                    presenter.login(email: email, password: password)
+        NavigationStack {
+            // TODO: 本番のView要素を適用させる
+            VStack(spacing: 20) {
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                // Loading状態の場合はIndicatorを表示し、そうでない時はログインボタンを表示する
+                if presenter.isLoading {
+                    ProgressView()
+                } else {
+                    Button("Login") {
+                        presenter.login(email: email, password: password)
+                    }
+                    .disabled(email.isEmpty || password.isEmpty)
                 }
-                .disabled(email.isEmpty || password.isEmpty)
+                
+                // もしエラーが発生した場合にはエラーメッセージを表示する
+                if let errorMessage = presenter.errorMessage {
+                    Text(errorMessage)
+                        .foregroundColor(.red)
+                }
             }
-
-            // もしエラーが発生した場合にはエラーメッセージを表示する
-            if let errorMessage = presenter.errorMessage {
-                Text(errorMessage)
-                    .foregroundColor(.red)
+            .padding()
+            .onFirstAppear {
+                // 初回表示時に1度だけ認証処理を実行する
+                presenter.checkAuthenticationStatus()
             }
-        }
-        .padding()
-        .onFirstAppear {
-            // 初回表示時に1度だけ認証処理を実行する
-            presenter.checkAuthenticationStatus()
+            // Navigation表示に関する設定
+            .navigationTitle("Login")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
