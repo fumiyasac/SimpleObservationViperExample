2a879efca397885d49bd9caec362c395e5ae2d9a
fumiyasac <just1factory@gmail.com>
2025-01-05 22:55:36 +0900

    InformationEntityを定義する

--- iOS/ObservationViperExample/ObservationViperExample/Entity/InformationFeedEntity.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Entity/InformationFeedEntity.swift
@@ -6,5 +6,67 @@
 //
 
 struct InformationFeedEntity: Hashable {
-    // TODO: 必要なPropertyをこれから決めていく
+
+    // MARK: - Property
+    
+    let id: Int
+    let title: String
+    let catchCopy: String
+    let summary: String
+    let thumbnail: String
+    
+    // MARK: - Enum
+
+    private enum Keys: String, CodingKey {
+        case id
+        case title
+        case catchCopy = "catch"
+        case summary
+        case thumbnail
+    }
+
+    // MARK: - Initializer
+
+    init(
+        id: Int,
+        title: String,
+        catchCopy: String,
+        summary: String,
+        thumbnail: String
+    ) {
+        self.id = id
+        self.title = title
+        self.catchCopy = catchCopy
+        self.summary = summary
+        self.thumbnail = thumbnail
+    }
+
+    init(from decoder: Decoder) throws {
+
+        // JSONの配列内の要素を取得する
+        let container = try decoder.container(keyedBy: Keys.self)
+
+        // JSONの配列内の要素にある値をDecodeして初期化する
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.catchCopy = try container.decode(String.self, forKey: .catchCopy)
+        self.summary = try container.decode(String.self, forKey: .summary)
+        self.thumbnail = try container.decode(String.self, forKey: .thumbnail)
+    }
+
+    // MARK: - Hashable
+
+    // MEMO: Hashableプロトコルに適合させるための処理
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    static func == (lhs: InformationFeedEntity, rhs: InformationFeedEntity) -> Bool {
+        return lhs.id == rhs.id
+            && lhs.title == rhs.title
+            && lhs.catchCopy == rhs.catchCopy
+            && lhs.summary == rhs.summary
+            && lhs.thumbnail == rhs.thumbnail
+    }
 }
