b36173e5632ee3d2769e81fea0b524ff1cbdb972
fumiyasac <just1factory@gmail.com>
2025-01-05 22:46:38 +0900

    該当Entityに必要なものを追加する

--- iOS/ObservationViperExample/ObservationViperExample/Entity/ApplicationUserEntity.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Entity/ApplicationUserEntity.swift
@@ -5,10 +5,20 @@
 //  Created by 酒井文也 on 2024/11/22.
 //
 
-struct ApplicationUserEntity: Hashable {
+struct ApplicationUserEntity {
 
     // MARK: - Property
 
     let email: String
     let token: String
+
+    // MARK: - Initializer
+
+    init(
+        email: String,
+        token: String
+    ) {
+        self.email = email
+        self.token = token
+    }
 }
--- iOS/ObservationViperExample/ObservationViperExample/Entity/GalleryPhotoEntity.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Entity/GalleryPhotoEntity.swift
@@ -5,6 +5,56 @@
 //  Created by 酒井文也 on 2024/11/11.
 //
 
-struct GalleryPhotoEntity: Hashable {
-    // TODO: 必要なPropertyをこれから決めていく
+struct GalleryPhotoEntity: Hashable, Decodable {
+
+    // MARK: - Property
+    
+    let id: Int
+    let title: String
+    let thumbnail: String
+    
+    // MARK: - Enum
+
+    private enum Keys: String, CodingKey {
+        case id
+        case title
+        case thumbnail
+    }
+
+    // MARK: - Initializer
+
+    init(
+        id: Int,
+        title: String,
+        thumbnail: String
+    ) {
+        self.id = id
+        self.title = title
+        self.thumbnail = thumbnail
+    }
+
+    init(from decoder: Decoder) throws {
+
+        // JSONの配列内の要素を取得する
+        let container = try decoder.container(keyedBy: Keys.self)
+
+        // JSONの配列内の要素にある値をDecodeして初期化する
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.thumbnail = try container.decode(String.self, forKey: .thumbnail)
+    }
+
+    // MARK: - Hashable
+
+    // MEMO: Hashableプロトコルに適合させるための処理
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    static func == (lhs: GalleryPhotoEntity, rhs: GalleryPhotoEntity) -> Bool {
+        return lhs.id == rhs.id
+            && lhs.title == rhs.title
+            && lhs.thumbnail == rhs.thumbnail
+    }
 }
