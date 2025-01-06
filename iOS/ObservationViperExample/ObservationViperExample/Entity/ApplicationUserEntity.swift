faf3a157ec9014f1802a52f54755cd444548d743
fumiyasac <just1factory@gmail.com>
2025-01-06 23:49:30 +0900

    ApplicationUserEntityをHashable & Decodableに準拠する

--- iOS/ObservationViperExample/ObservationViperExample/Entity/ApplicationUserEntity.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Entity/ApplicationUserEntity.swift
@@ -5,12 +5,23 @@
 //  Created by 酒井文也 on 2024/11/22.
 //
 
-struct ApplicationUserEntity {
+import Foundation
+
+struct ApplicationUserEntity: Hashable, Decodable {
 
     // MARK: - Property
 
+    private let uuid = UUID()
+
     let email: String
     let token: String
+
+    // MARK: - Enum
+
+    private enum Keys: String, CodingKey {
+        case email
+        case token
+    }
 
     // MARK: - Initializer
 
@@ -20,5 +31,28 @@ struct ApplicationUserEntity {
     ) {
         self.email = email
         self.token = token
+    }
+
+    init(from decoder: Decoder) throws {
+
+        // JSONの配列内の要素を取得する
+        let container = try decoder.container(keyedBy: Keys.self)
+
+        // JSONの配列内の要素にある値をDecodeして初期化する
+        self.email = try container.decode(String.self, forKey: .email)
+        self.token = try container.decode(String.self, forKey: .token)
+    }
+
+    // MARK: - Hashable
+
+    // MEMO: Hashableプロトコルに適合させるための処理
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uuid)
+    }
+
+    static func == (lhs: ApplicationUserEntity, rhs: ApplicationUserEntity) -> Bool {
+        return lhs.email == rhs.email
+            && lhs.token == rhs.token
     }
 }
