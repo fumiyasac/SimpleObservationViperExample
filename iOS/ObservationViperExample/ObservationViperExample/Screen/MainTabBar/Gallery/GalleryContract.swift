218ea9af5e326be00c930b76ffab9b219c81f13e
fumiyasac <just1factory@gmail.com>
2025-01-07 23:36:04 +0900

    GalleryContractファイルを新規追加

---
+++ iOS/ObservationViperExample/ObservationViperExample/Screen/MainTabBar/Gallery/GalleryContract.swift
@@ -0,0 +1,21 @@
+//
+//  GalleryContract.swift
+//  ObservationViperExample
+//
+//  Created by 酒井文也 on 2025/01/07.
+//
+
+import Foundation
+
+// MARK: - Protocol
+
+protocol GalleryPresenterProtocol {
+    var isLoading: Bool { get }
+    var errorMessage: String? { get }
+    var galleries: [GalleryPhotoEntity] { get }
+    func fetchGalleries() async throws -> [GalleryPhotoEntity]
+}
+
+protocol GalleryInteractorProtocol {
+    func fetchGalleries() async throws -> [GalleryPhotoEntity]
+}
