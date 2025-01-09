a9dded942759d8c62b271fb09add12003a7b5efc
fumiyasac <just1factory@gmail.com>
2025-01-09 21:42:57 +0900

    FeedContractを新規追加

---
+++ iOS/ObservationViperExample/ObservationViperExample/Screen/MainTabBar/Feed/FeedContract.swift
@@ -0,0 +1,26 @@
+//
+//  FeedContract.swift
+//  ObservationViperExample
+//
+//  Created by 酒井文也 on 2025/01/09.
+//
+
+import Foundation
+
+// MARK: - Protocol
+
+protocol FeedPresenterProtocol {
+    var isLoading: Bool { get }
+    var errorMessage: String? { get }
+    var pickupFeeds: [PickupFeedEntity] { get }
+    var categoryRankings: [CategoryRankingEntity] { get }
+    var informationFeeds: [InformationFeedEntity] { get }
+    func fetchInitialFeeds()
+    func fetchNextInformationFeeds()
+}
+
+protocol FeedInteractorProtocol {
+    func fetchPickupFeeds() async throws -> [PickupFeedEntity]
+    func fetchCategoryRankings() async throws -> [CategoryRankingEntity]
+    func fetchInformationFeeds() async throws -> InformationFeedPageEntity
+}
