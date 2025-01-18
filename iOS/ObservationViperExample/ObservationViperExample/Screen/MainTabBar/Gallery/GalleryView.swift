902a1e130c2bd5ebf327786e30741ba404a228f0
fumiyasac <just1factory@gmail.com>
2025-01-18 10:03:03 +0900

    画面表示処理ハンドリングを実施する

--- iOS/ObservationViperExample/ObservationViperExample/Screen/MainTabBar/Gallery/GalleryView.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Screen/MainTabBar/Gallery/GalleryView.swift
@@ -31,17 +31,33 @@ struct GalleryView: View {
     var body: some View {
         // MEMO: UIKitのNavigationController表示を実施する
         NavigationStack {
-            ZStack {
-                ScrollView {
-                    LazyVGrid(columns: gridColumns, spacing: 1.0) {
-                        ForEach(presenter.galleryPhotos, id: \.id) { galleryPhoto in
-                            LazyImage(url: URL(string: galleryPhoto.thumbnail)) { imageState in
-                                if let image = imageState.image {
-                                    image
-                                        .resizable()
-                                        .aspectRatio(1, contentMode: .fill)
-                                } else {
-                                    Color(uiColor: UIColor(code: "#dddddd"))
+            Group {
+                switch (presenter.isLoading, presenter.errorMessage) {
+                case (true, _):
+                    // Loading Indicatorを表示する
+                    ExecutingConnectionView()
+                case (_, presenter.errorMessage) where presenter.errorMessage != nil:
+                    // Error Message画面を表示する
+                    ConnectionErrorView(
+                        tapButtonAction: {
+                            presenter.fetchGalleryPhotos()
+                        }
+                    )
+                default:
+                    // Grid表示を伴うGallery画面を表示する
+                    ZStack {
+                        ScrollView {
+                            LazyVGrid(columns: gridColumns, spacing: 1.0) {
+                                ForEach(presenter.galleryPhotos, id: \.id) { galleryPhoto in
+                                    LazyImage(url: URL(string: galleryPhoto.thumbnail)) { imageState in
+                                        if let image = imageState.image {
+                                            image
+                                                .resizable()
+                                                .aspectRatio(1, contentMode: .fill)
+                                        } else {
+                                            Color(uiColor: UIColor(code: "#dddddd"))
+                                        }
+                                    }
                                 }
                             }
                         }
