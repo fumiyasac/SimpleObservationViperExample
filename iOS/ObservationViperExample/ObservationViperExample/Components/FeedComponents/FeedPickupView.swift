9f5df5dea1c94c4b6af6857ea88f55706b379d8a
fumiyasac <just1factory@gmail.com>
2025-01-18 10:12:41 +0900

    FeedPickupViewの新規追加

---
+++ iOS/ObservationViperExample/ObservationViperExample/Components/FeedComponents/FeedPickupView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedPickupView.swift
+//  ObservationViperExample
+//
+//  Created by 酒井文也 on 2025/01/18.
+//
+
+import SwiftUI
+
+struct FeedPickupView: View {
+
+    // MARK: - Body
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedPickupView()
+}
