de21a23c65871bda1ea7ef0ba8a603cef5f6e628
fumiyasac <just1factory@gmail.com>
2025-01-18 11:18:51 +0900

    CategoryFeedViewと対応するJSONの新規追加

---
+++ iOS/ObservationViperExample/ObservationViperExample/Components/FeedComponents/CategoryFeedView.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryFeedView.swift
+//  ObservationViperExample
+//
+//  Created by 酒井文也 on 2025/01/18.
+//
+
+import SwiftUI
+
+struct CategoryFeedView: View {
+
+    // MARK: - Body
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryFeedView()
+}
