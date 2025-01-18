6116a305c016d096e76772de5123fc64a3f91ba1
fumiyasac <just1factory@gmail.com>
2025-01-18 18:22:13 +0900

    AuthenticationViewのデザイン適用

--- iOS/ObservationViperExample/ObservationViperExample/Screen/Authentication/AuthenticationView.swift
+++ iOS/ObservationViperExample/ObservationViperExample/Screen/Authentication/AuthenticationView.swift
@@ -28,28 +28,51 @@ struct AuthenticationView: View {
 
     var body: some View {
         NavigationStack {
-            // TODO: 本番のView要素を適用させる
-            VStack(spacing: 20) {
-                TextField("Email", text: $email)
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Eメールアドレス:")
+                    .font(.callout)
+                    .bold()
+                    .foregroundStyle(.gray)
+                TextField("Email:", text: $email)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .autocapitalization(.none)
-                SecureField("Password", text: $password)
+                Text("パスワード:")
+                    .font(.callout)
+                    .bold()
+                    .foregroundStyle(.gray)
+                SecureField("Password:", text: $password)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                // Loading状態の場合はIndicatorを表示し、そうでない時はログインボタンを表示する
-                if presenter.isLoading {
-                    ProgressView()
-                } else {
-                    Button("Login") {
-                        presenter.login(email: email, password: password)
-                    }
-                    .disabled(email.isEmpty || password.isEmpty)
-                }
-                
+
                 // もしエラーが発生した場合にはエラーメッセージを表示する
                 if let errorMessage = presenter.errorMessage {
                     Text(errorMessage)
                         .foregroundColor(.red)
+                }
+
+                // Loading状態の場合はIndicatorを表示し、そうでない時はログインボタンを表示する
+                if presenter.isLoading {
+                    ProgressView()
+                } else {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            presenter.login(email: email, password: password)
+                        }, label: {
+                            Text("ログイン")
+                                .font(.body)
+                                .foregroundColor(Color(uiColor: UIColor(code: "#f88c75")))
+                                .background(.white)
+                                .frame(width: 240.0, height: 48.0)
+                                .cornerRadius(24.0)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 24.0)
+                                        .stroke(Color(uiColor: UIColor(code: "#f88c75")), lineWidth: 1.0)
+                                )
+                        })
+                        Spacer()
+                    }
+                    .disabled(email.isEmpty || password.isEmpty)
+                    .padding(.vertical, 8.0)
                 }
             }
             .padding()
